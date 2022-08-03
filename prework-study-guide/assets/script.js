e100b5c7e857b99f173f3993c1c90960e768d8e7
Kevin DeVore <kdevore10@gmail.com>
2022-08-02 20:16:26 -0400

    <Added Java and learned>

---
+++ prework-study-guide/assets/script.js
@@ -0,0 +1,32 @@
+
+
+var topics = ['HTML', 'CSS', 'Git', 'JavaScript'];
+
+for (var x = 0; x < topics.length; x++) {
+
+    console.log(topics[x]);
+}
+
+/*
+if (topic === 'HTML') {
+
+    console.log("Lets study HTML!");
+
+} else if (topic ==='CSS') {
+
+    console.log("Lets study CSS!");
+
+} else if (topic === 'GIT'){
+
+    console.log("Let's study Git!");
+
+} else if (topic === 'JavaScript'){
+
+    console.log("Let's study JavaScript");
+} else {
+
+    console.log('Please try again!');
+}
+*/
+
+
